bae74c2510dfed6ce20499bf164d12804cc74530
Schuster <brian.daniel.schuster@gmail.com>
2022-02-13 00:27:27 -0500

    PK account now responsible for executing the transaction

--- src/lib/useEthers.js
+++ src/lib/useEthers.js
@@ -4,7 +4,7 @@ import { ethers } from 'ethers'
 import contract from '../contracts/SchusterEtherFaucet.json'
 import { isWeb3Available } from './web3.utils'
 
-const { REACT_APP_FAUCET_CONTRACT_ADDRESS } = process.env
+const { REACT_APP_FAUCET_CONTRACT_ADDRESS, REACT_APP_METAMASK_TEST_ACCOUNT_3_PRIK} = process.env
 
 export const useEthers = () => {
   const [state, setState] = useState({
@@ -19,15 +19,19 @@ export const useEthers = () => {
     try {
       if (isWeb3Available) {
         setState({ ...state, connecting: true, error: '' })
+        //Connects to the local instance of Metamask
         const provider = new ethers.providers.Web3Provider(window.ethereum)
         await provider.send('eth_requestAccounts', [])
         const addresses = await provider.listAccounts()
 
+        //Creates a Wallet from the Runner's Private Key and connects to the contract
+        const wallet = new ethers.Wallet(REACT_APP_METAMASK_TEST_ACCOUNT_3_PRIK, provider);
         const faucetContract = new ethers.Contract(
           REACT_APP_FAUCET_CONTRACT_ADDRESS,
           contract.abi,
-          provider
+          wallet
         )
+
         const contractBalance = await faucetContract.getBalance()
         const signer = await provider.getSigner()
 
